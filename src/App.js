266500c941b8ea956e6b12258fce2df98f4a57f5
oluwolesuraj89 <oluwolesuraj98@gmail.com>
2024-03-26 13:07:30 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Loans from './Admin/Loans/Loans';
 import Grants from './Admin/Grants/Grants';
 import ViewGrants from './Admin/Grants/ViewGrants';
 import ViewApplication from './Admin/Applications/ViewApplication';
+import AdminHome from './Admin/AdminHome/AdminHome';
 
 
 
@@ -79,7 +80,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/'element={<SignIn/>}/>
+        <Route path='/'element={<AdminHome/>}/>
         <Route path='/login'element={<SignIn/>}/>
         <Route path='/admin'element={<Admin/>}/>
         <Route path='/applications'element={<Applications/>}/>
